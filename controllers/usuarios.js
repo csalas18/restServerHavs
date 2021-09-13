e08f80699803309f529cd03f8f574771de1f5bfe
csalas <csalas25@gmail.com>
2021-09-13 19:42:55 -0300

    commit Inicial

---
+++ controllers/usuarios.js
@@ -0,0 +1,58 @@
+const {response, request} = require('express')
+
+const usuariosGet  = (req = request, res = response) => {
+
+    const query = req.query;
+   
+    res.json({
+        msg:'get API - Controlador',
+        query
+    })
+}
+
+const usuariosPut  = (req, res = response) => {
+   
+    const id = req.params.id;
+
+    res.json({
+        msg:'put API - Controlador',
+        id
+    })
+}
+
+const usuariosPost  = (req, res = response) => {
+   
+     const body = req.body;
+
+
+    res.json({
+        msg:'post API - Controlador',
+        body
+    })
+}
+
+const usuariosDelete  = (req, res = response) => {
+   
+
+    res.json({
+        msg:'delete API - Controlador'
+    })
+}
+
+const usuariosPatch  = (req, res = response) => {
+   
+    res.json({
+        msg:'patch API - Controlador'
+    })
+}
+
+
+
+
+module.exports={
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
